4dd46597bd04453dd988851dae4c401253c90f95
James Moore <jcmmars@gmail.com>
2016-05-24 22:31:14 -0400

    complete session count example

--- example/main.go
+++ example/main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/lumost/traildb-go"
 )
 
-type Ev struct {
-	Timestamp int    `tdb:"timestamp"`
-	Field1    string `tdb:"field1"`
-	Field2    string `tdb:"field2"`
-}
+const SESSION_LIMIT = 30
 
 type WikiEvent struct {
-	Timestamp int64  `tdb:"timestamp"`
+	TimeStamp int64  `tdb:"timestamp"`
 	User      string `tdb:"user"`
 	IP        string `tdb:"ip"`
 	Title     string `tdb:"title"`
@@ -26,25 +23,38 @@ func main() {
 	}
 	fmt.Println(db)
 	fmt.Println(db.Version())
+	countSessions(db)
 
-	var total int
-	for i := 0; i < 10; i++ {
+}
+
+func countSessions(db *tdb.TrailDB) {
+
+	for i := 0; i < db.NumTrails; i++ {
 		trail, err := tdb.NewTrail(db, i)
 		if err != nil {
 			panic(err.Error())
 		}
-		// fmt.Println(trail)
+		//Trail Scoped session counter for number of sessions per user
+		numSessions := 0
+		numEvents := 0
+		prevTime := int64(0)
 		for {
 			evt := trail.NextEvent()
 			if evt == nil {
 				trail.Close()
 				break
 			}
-			total++
-			fmt.Println(evt.ToMap())
-			// Convert to struct
 			we := WikiEvent{}
-			fmt.Printf("struct format %+v", evt.ToStruct(we))
+			// cast to event to struct
+			//  then assert that it is reflect.Value
+			//  then use Interface() to get an interface for the value
+			/// then type assert to a pointer of the original WikiEvent type
+			parsedWikiEvent := evt.ToStruct(we).(reflect.Value).Interface().(*WikiEvent)
+			if parsedWikiEvent.TimeStamp-prevTime > SESSION_LIMIT {
+				numSessions++
+			}
+			prevTime = parsedWikiEvent.TimeStamp
 		}
+		fmt.Printf("Number of Sessions: %d Number of Events: %d\n", numSessions, numEvents)
 	}
 }
